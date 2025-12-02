424234d4396ddd73d3b4aed5c544d2444a20fa50
Nevo David <me@nevos.io>
2025-12-02 21:33:03 +0700

    feat: crawlchat

---
+++ crawlchat.js
@@ -0,0 +1,10 @@
+function loadCrawlChat() {
+  const script = document.createElement('script');
+  script.src = "https://crawlchat.app/embed.js";
+  script.id = "crawlchat-script";
+  script.dataset.id = "68ac269d2961657c4b7924a9";
+
+  document.head.appendChild(script);
+}
+
+loadCrawlChat();
